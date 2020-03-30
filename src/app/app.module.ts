fc03fe96ce2f9d1c1810213341e7fb43358992e2
Chalanika <chalanikasewwandi@gmail.com>
2020-03-30 17:51:24 +0530

    create order-list component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,13 +13,15 @@ import { NavbarComponent } from './modules/navbar/navbar.component';
 import { MatModule } from './core/mat/mat.module';
 import { ChatComponent } from './modules/chat/chat.component';
 import { FormsModule } from '@angular/forms';
+import { OrderListComponent } from './modules/orders/order-list/order-list.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    ChatComponent
+    ChatComponent,
+    OrderListComponent
   ],
   imports: [
     BrowserModule,
