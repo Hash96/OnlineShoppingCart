bbb48e9c4cd2dd382fc0abb1d4de602e18fe8e44
Chalanika <chalanikasewwandi@gmail.com>
2020-04-20 13:46:13 +0530

    finished sign up

--- src/app/core/services/user.service.ts
+++ src/app/core/services/user.service.ts
@@ -10,11 +10,12 @@ import { AngularFireAuth } from '@angular/fire/auth';
 export class UserService {
 
   user;
+  x;
 
   constructor(private afAuth: AngularFireAuth, private db: AngularFirestore) {
     this.getCurrentUser();
   }
-//get current user who has logged
+  //get current user who has logged
   getCurrentUser() {
     this.afAuth.authState.subscribe(
       (user) => {
@@ -27,46 +28,72 @@ export class UserService {
             }
           )
         }
-      },(err)=>{
+      }, (err) => {
         console.log(err);
       }
     )
   }
-//google
-  googleLogin(){
-    let x= this.afAuth.signInWithPopup(new auth.GoogleAuthProvider()).then(
-      (cred)=>{
-        if(cred.user){
-          this.db.collection("users").doc(cred.user.uid).valueChanges().subscribe(
-            (currentUser)=>{
-              if(!currentUser){
-                this.db.collection("users").doc(cred.user.uid).set({
-                  Name :cred.user.displayName,
-                  Email : cred.user.email,
-                  Profile:cred.user.photoURL,
-                  Contact : cred.user.phoneNumber 
-                })
-              }else{
-                console.log(currentUser);
-              }
-            }
-          )
+  //google
+  googleLogin() {
+    let x = this.afAuth
+      .signInWithPopup(new auth.GoogleAuthProvider())
+      .then(
+        (cred) => {
+          if (cred.user) {
+            this.db
+              .collection("users")
+              .doc(cred.user.uid)
+              .valueChanges()
+              .subscribe(
+                (currentUser) => {
+                  if (!currentUser) {
+                    this.db.collection("users").doc(cred.user.uid).set({
+                      Name: cred.user.displayName,
+                      Email: cred.user.email,
+                      Profile: cred.user.photoURL,
+                      Contact: cred.user.phoneNumber
+                    })
+                  } else {
+                    console.log(currentUser);
+                  }
+                }
+              )
+          }
         }
-      }
-    )
+      )
     return x;
   }
 
-//logout
-logout(){
-  let x = this.afAuth.signOut(); 
-  console.log("ppppp");
-  this.user = null;
-  return x;
+  //logout
+  logout() {
+    let x = this.afAuth.signOut();
+    console.log("ppppp");
+    this.user = null;
+    return x;
+  }
+
+  //sign up
+  signUp(userDetails) {
+    this.x = userDetails;
+    let cred = this.afAuth.createUserWithEmailAndPassword(userDetails.email, userDetails.password);
+    return cred;
+    
+  }
+
+  chechDbUser(userDetails) {
+    console.log(userDetails);
+    this.db.collection("users").doc(userDetails.uid).set({
+      Name: this.x.name,
+      Email: this.x.email,
+      Profile: userDetails.photoURL,
+      Contact:userDetails.phoneNumber
+  });
 }
 
-//sign up
-signUp(){
-
+/* Sign in */
+login(userDetails) {
+  let x =this.afAuth.signInWithEmailAndPassword(userDetails.email, userDetails.password);
+  return x;
+    
 }
 }
--- src/app/modules/login/login.component.ts
+++ src/app/modules/login/login.component.ts
@@ -1,18 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from 'src/app/core/services/user.service';
 import { Router } from '@angular/router';
+import { User } from 'src/app/core/models/User';
+
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
+
 export class LoginComponent implements OnInit {
+
   signUp=false;
+  userModel =  new User();
+  error = 0;
+ 
   constructor(public _userService:UserService,private router:Router) { }
 
   ngOnInit(): void {
   }
+  
 
   moveSignUp(){
     this.signUp = true;
@@ -32,6 +40,33 @@ export class LoginComponent implements OnInit {
     )
   }
 
- 
+ register(){
+   this._userService.signUp(this.userModel).then(
+     (res)=>{
+      console.log(res.user.uid);
+      this.insertUser(res.user);   
+     },(err)=>{
+       console.log("already have an account");
+       this.signUp=false;
+       this.error=1;
+     }
+   )
+ }
 
+insertUser(user){
+  this._userService.chechDbUser(user);
+  this.router.navigate(['/']);
+ }
+
+ login(){
+   this._userService.login(this.userModel).then(
+     (res)=>{
+       console.log(res);
+       this.router.navigate(['/']);
+     },(err)=>{
+       console.log(err);
+       this.error=2;
+     }
+   )
+ }
 }
